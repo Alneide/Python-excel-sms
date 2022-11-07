a84bdbc2a6ca00466f0cb980017e195fe1a0a9b5
Alneide Oliveira <alneide@gmail.com>
2022-11-07 13:12:50 -0300

    Busca de arquivos do excel

---
+++ Pithonexcelsms.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# passo a passo
+
+# Abrir os 6 arquivos em Excel
+Lista_meses = ['janeiro', 'fevereiro', 'março','abril',"maio", "junho"]
+
+for mes in lista_meses :
+    print(mes)
+    tabela_vendas = pd.read_excel (f"{mes}.xlsx")
+    print(tabela_vendas)

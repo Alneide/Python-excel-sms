86e2019f5d5bad6f7297ad50971ceefb60638a3f
Alneide Oliveira <alneide@gmail.com>
2022-11-08 13:10:33 -0300

    Mostrando vendedor que bateu meta, resultado dinâmico do Python

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ Lista_meses = ['janeiro', 'fevereiro', 'março','abril',"maio", "junho"]
 for mes in lista_meses :
     tabela_vendas = pd.read_excel (f"{mes}.xlsx")
     if (tabela_vendas["vendas"]>55000).any():
-        vendedor = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendedor"]
-        vendas = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendas"]
+        vendedor = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendedor"].values[0]
+        vendas = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendas"].values[0]
 
-    print (f"No mês{mes}encontrou alguém com mais de 55000")
+    print (f"No mês{mes} alguém bateu a meta. Vendedor:  {Vendedor}, Vendas: {Vendas}")
+
 

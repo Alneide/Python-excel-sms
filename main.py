200a4cbb1e12600af9b7ff172e496c6558fe7d52
Alneide Oliveira <alneide@gmail.com>
2022-11-08 10:18:37 -0300

    importar resultado com maior venda

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import pandas as pd
 Lista_meses = ['janeiro', 'fevereiro', 'março','abril',"maio", "junho"]
 
 for mes in lista_meses :
-    print(mes)
     tabela_vendas = pd.read_excel (f"{mes}.xlsx")
-    print (tabela_vendas)
+    if (tabela_vendas["vendas"]>55000).any():
+        vendedor = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendedor"]
+        vendas = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendas"]
+
+    print (f"No mês{mes}encontrou alguém com mais de 55000")
+

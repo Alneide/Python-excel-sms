560b888bc1cf4d4dcb7bf05db0c84a3b8f4705d0
Alneide Oliveira <alneide@gmail.com>
2022-11-09 19:14:05 -0300

    Importando o Twilio para envio de sms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from twilio.rest import Client
 
 # passo a passo
 
@@ -12,5 +13,20 @@ for mes in lista_meses :
         vendas = tabela_vendas.loc[tabela - vendas["vendas"] > 55000, "Vendas"].values[0]
 
     print (f"No mês{mes} alguém bateu a meta. Vendedor:  {Vendedor}, Vendas: {Vendas}")
+
+ 
+    # Your Account SID from twilio.com/console
+    account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    # Your Auth Token from twilio.com/console
+    auth_token = "your_auth_token"
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to="+15558675309",
+        from_="+15017250604",
+        body="Hello from Python!")
+
+    print(message.sid)
 
 
